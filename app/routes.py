f6ac763fd3372dfe1d81628cf7ee0822fcbd247a
Thibault Douzon <douzont@gmail.com>
2020-05-02 04:54:34 +0200

    FEAT007: better form and data validation

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,8 @@
 from app import app
 
-from flask import render_template
+from flask import render_template, request
 
-@app.route("/")
-
+@app.route("/", methods=["GET"])
 def index():
     flower_types = [
         {"name": "Cosmos"},
@@ -17,7 +16,16 @@ def index():
     
     return render_template("form.html", title="Home", flower_types=flower_types, flower_colors=flower_colors)
 
-
+@app.route("/results", methods=["POST"])
+def result_page():
+    form = request.form
+    
+    tgt_type = form["tgt_type"]
+    tgt_color = form["tgt_color"]
+    seed = True if "seed" in form else False
+    island = True if "island" in form else False
+    
+    return f"{tgt_type=} {tgt_color=} {seed=} {island=}"
 
 
 
